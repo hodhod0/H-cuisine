011f1d8c3d18085505ce6acef15e2912b0a595a3
abdelhafiz <iaali.abdelhafiz@outlook.com>
2022-07-10 01:10:37 +0300

    css intro

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,26 @@
-import './App.css';
-import React from 'react';
-import Login from './pages/login/Login';
-import Home from './pages/home/Home';
-import Register from './pages/register/Register';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import React from "react";
+import Login from "./pages/login/Login";
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          {localStorage.getItem("token") !== null ?
-            <Route path='/home' element={<Home />} /> :
-            <Route path="/login" element={<Login />} />
-          }
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        {localStorage.getItem("token") !== null ? (
+          <Route path="/home" element={<Home />} />
+        ) : (
           <Route path="/login" element={<Login />} />
+        )}
+        <Route path="/login" element={<Login />} />
 
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
